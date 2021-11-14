e3b6e03e5a131cc2b014d2d9811f4e2e07d56d1e
Prekra <77902653+Prekra@users.noreply.github.com>
2021-11-13 22:47:58 -0500

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,104 @@
+function mostrar(){
+
+swal({
+    title:"",
+    text:"Estás guapisima",
+    icon:"./img/1.jpg",
+   
+})
+
+
+a=setTimeout(dos,2000)
+b=setTimeout(tres,3000)
+c=setTimeout(cuatro,4000)
+d=setTimeout(cinco,5000)
+e=setTimeout(seis,6000)
+f=setTimeout(siete,7000)
+
+}
+
+function dos(){
+
+    swal({
+        title:"",
+        text:"Estás guapisima",
+        icon:"./img/2.jpg",
+       
+     
+        
+    }) 
+
+  
+    
+}
+function tres(){
+
+    swal({
+        title:"",
+        text:"Estás guapisima",
+        icon:"./img/3.jpg",
+       
+     
+        
+    }) 
+
+    
+    
+}
+function cuatro(){
+
+    swal({
+        title:"",
+        text:"Estás guapisima",
+        icon:"./img/4.jpg",
+       
+     
+        
+    }) 
+
+  
+    
+}
+
+function cinco(){
+
+    swal({
+        title:"",
+        text:"Estás guapisima",
+        icon:"./img/5.jpg",
+       
+     
+        
+    }) 
+
+  
+    
+}
+function seis(){
+
+    swal({
+        title:"",
+        text:"Estás guapisima",
+        icon:"./img/6.jpg",
+       
+     
+        
+    }) 
+
+  
+    
+}
+function siete(){
+
+    swal({
+        title:"",
+        text:"Estás guapisima",
+        icon:"./img/7.jpg",
+       
+     
+        
+    }) 
+
+  
+    
+}
